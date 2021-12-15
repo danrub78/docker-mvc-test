8e01d122a665ae603be656af9c41539f23804f43
Dante Rubinaccio <dante@MBP-di-Dante.home>
2021-12-15 11:54:21 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace docker_mvc_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
