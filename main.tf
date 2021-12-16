d7a052d9975dfc95bcb183eaaa2560ced2db96ce
Dante Rubinaccio <dante@MBP-di-Dante.home>
2021-12-16 15:36:31 +0100

    update pipeline

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+  backend "azurerm" {
+      resource_group_name = "the_rg_on_azure"
+      storage_account_name = "the_storage_account_nome"
+      container_name = "the_blob_container_name"
+      key = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "docker_mvc_rg" {
   name     = "docker-mvc-resources"
   location = "West Europe"
